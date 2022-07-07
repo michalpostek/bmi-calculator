77041cf8a10430db815a6b897161ed80170a877c
Michał Postek <postek.michal@protonmail.com>
2022-07-07 15:39:15 +0200

    show results in table

--- js/helpers.js
+++ js/helpers.js
@@ -2,7 +2,7 @@ const getCategory = (BMI) => {
     if (BMI < 18.5) {
         return 'Underweight';
     } else if (BMI < 25) {
-        return 'healthy weight';
+        return 'Healthy weight';
     } else if (BMI < 30) {
         return 'Overweight';
     } else {
--- js/index.js
+++ js/index.js
@@ -1,34 +1,41 @@
 const weightInput = document.getElementById('weight');
 const heightInput = document.getElementById('height');
-const resultOutput = document.getElementById('result');
+
+const resultTable = document.getElementById('resultTable');
+const bmiOutput = document.getElementById('bmiOutput');
+const categoryOutput = document.getElementById('categoryOutput');
+const errorOutput = document.getElementById('error')
+
 const getBmiBtn = document.getElementById('getBmiBtn');
 const clearBtn = document.getElementById('clearBtn');
 
 getBmiBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    let result;
-
     const weight = parseFloat(weightInput.value);
     const height = parseFloat(heightInput.value);
 
     if (!weight || !height) {
-        result = 'You passed incorrect values!';
+        resultTable.classList.add('hidden');
+        errorOutput.classList.remove('hidden');
+
+        errorOutput.textContent = 'You passed incorrect values';
     } else {
         const BMI = Math.round(weight / Math.pow(height / 100, 2) * 100) / 100;
         const category = getCategory(BMI);
 
-        result = `
-            <p>Your BMI is ${BMI} </p>
-            <p>Your Category is ${category} </p>
-        `
-    }
+        resultTable.classList.remove('hidden');
+        errorOutput.classList.add('hidden');
 
-    resultOutput.innerHTML = result;
+        bmiOutput.textContent = BMI;
+        categoryOutput.textContent = category;
+    }
 });
 
 clearBtn.addEventListener('click', () => {
     weightInput.value = '';
     heightInput.value = '';
-    resultOutput.innerHTML = '';
+    
+    errorOutput.classList.add('hidden');
+    resultTable.classList.add('hidden');
 });
